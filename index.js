b0e4af7a216ee706ff53d3890117ed5ea181180a
KrnmSaiKrishna <38278654+krnmsaikrishna@users.noreply.github.com>
2022-10-17 10:48:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,25 +2,21 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 const port = process.env.PORT || 3131;
 const IS_PRODUCTION = process.env.NODE_ENV === 'production';
-
 const app = express();
-
-// ok
-
 const getBrowser = () =>
   IS_PRODUCTION
     ? puppeteer.connect({ browserWSEndpoint: 'wss://chrome.browserless.io?token=968d84d4-8532-425a-91b1-dae9ba8a1b46' })
     : puppeteer.launch();
 
-app.get('/image', async (req, res) => {
+app.get('/source', async (req, res) => {
   let browser = null;
+  let url = decodeURI(req.query.url);
 
   try {
     browser = await getBrowser();
     const page = await browser.newPage();
-
-    await page.goto('https://www.gktoday.in/quizbase/current-affairs-quiz-january-2022?pageno=1');
-    const data = await page.evaluate(() => document.querySelector('*').outerHTML);
+    await page.goto(url);
+    const data = await page.evaluate(() => document.querySelector('quiz_print').outerHTML);
     await browser.close();
     res.send(data);
     
